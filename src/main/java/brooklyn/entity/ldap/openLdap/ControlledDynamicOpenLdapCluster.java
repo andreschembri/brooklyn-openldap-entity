ce18eee933b2b198f71b1cb44795ece2818335f0
Andre <andre.schembri@cloudsoftcorp.com>
2015-07-30 11:39:42 +0200

    removed extra line break and unused imports

--- src/main/java/brooklyn/entity/ldap/openLdap/ControlledDynamicOpenLdapCluster.java
+++ src/main/java/brooklyn/entity/ldap/openLdap/ControlledDynamicOpenLdapCluster.java
@@ -2,29 +2,18 @@ package brooklyn.entity.ldap.openLdap;
 
 import brooklyn.catalog.Catalog;
 import brooklyn.config.ConfigKey;
-import brooklyn.entity.Entity;
-import brooklyn.entity.annotation.Effector;
-import brooklyn.entity.basic.Attributes;
 import brooklyn.entity.basic.ConfigKeys;
-import brooklyn.entity.basic.ConfigurableEntityFactory;
-import brooklyn.entity.basic.Lifecycle;
 import brooklyn.entity.group.Cluster;
 import brooklyn.entity.group.DynamicCluster;
-import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.entity.proxying.ImplementedBy;
 import brooklyn.event.AttributeSensor;
-import brooklyn.event.basic.BasicAttributeSensor;
 import brooklyn.event.basic.BasicAttributeSensorAndConfigKey;
 import brooklyn.event.basic.Sensors;
 import brooklyn.util.flags.SetFromFlag;
-import com.google.common.reflect.TypeToken;
-
-import java.util.Map;
 
 @Catalog(name = "Controlled Dynamic OpenLDAP Cluster", description = "A cluster of OpenLDAP servers, which can be dynamically resized")
 @ImplementedBy(ControlledDynamicOpenLdapClusterImpl.class)
 public interface ControlledDynamicOpenLdapCluster extends DynamicCluster {
-
 
     @SetFromFlag("initialSize")
     ConfigKey<Integer> INITIAL_SIZE = ConfigKeys.newConfigKeyWithDefault(Cluster.INITIAL_SIZE, 1);
